cd114e59323e716d50cd23776389369e7a5db9e3
evgenius1424 <evgenius1424@yandex.ru>
2022-03-17 16:14:48 +0300

    fix integration test

--- server/src/integrationTest/kotlin/com/github/evgenius1424/CompanyIntegrationTest.kt
+++ server/src/integrationTest/kotlin/com/github/evgenius1424/CompanyIntegrationTest.kt
@@ -2,14 +2,20 @@ package com.github.evgenius1424
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.koin.core.KoinApplication
 import org.koin.test.KoinTest
 import org.koin.test.get
+import org.koin.test.junit5.KoinTestExtension
 
 internal class CompanyIntegrationTest : KoinTest {
 
+    @RegisterExtension
+    @JvmField
+    val koinTestExtension = KoinTestExtension.create(KoinApplication::configureDI)
+
     @Test
     fun `DI smoke test`() {
-        initializeDI()
         val company = get<Company>()
         assertThat(company).isInstanceOf(SmartCompany::class.java)
     }
--- server/src/main/kotlin/com/github/evgenius1424/Application.kt
+++ server/src/main/kotlin/com/github/evgenius1424/Application.kt
@@ -5,18 +5,19 @@ import com.github.evgenius1424.plugins.configureRouting
 import com.github.evgenius1424.plugins.configureSerialization
 import io.ktor.application.Application
 import io.ktor.server.netty.EngineMain
+import org.koin.core.KoinApplication
 import org.koin.core.context.GlobalContext.startKoin
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
-    initializeDI()
+    startKoin {
+        configureDI()
+    }
     configureRouting()
     configureSerialization()
 }
 
-fun initializeDI() {
-    startKoin {
-        modules(applicationModule)
-    }
+fun KoinApplication.configureDI() {
+    modules(applicationModule)
 }
